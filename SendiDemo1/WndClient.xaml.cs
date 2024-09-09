98ff56fb60ffd74ad7bad458039cfca78037e9cd
Paul Coomans <paul.coomans@looi.be>
2024-09-09 15:34:12 +0200

    Rename from Messenger to Sendi (internal) Created demo project 'SendiDemo' Created unit test project 'TestSendi'

---
+++ SendiDemo1/WndClient.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Sendi.Client;
+using Sendi.Messages;
+using SendiDemo1.MessageTypes;
+using System.Windows;
+
+namespace SendiDemo1
+{
+	/// <summary>
+	/// Interaction logic for WndClient.xaml
+	/// </summary>
+	public partial class WndClient : Window
+    {
+        private IMessageClient messageClient;
+
+        public WndClient(IMessageClient msgClient)
+        {
+            InitializeComponent();
+            Width = 300;
+            Height = 150;
+
+            // connect to message bus
+            messageClient = msgClient;
+            messageClient.SetFilterMode(AbstractMessageClient.EnmFilterMode.Filter);
+            messageClient.AddMsgTypeToFilter(new MsgColor(null), MsgColorReceived);
+            messageClient.Start();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            messageClient.Stop();
+        }
+
+        public void MsgColorReceived(IMessage message)  
+        {
+			if (message is MsgColor msgColor)
+			{
+				Application.Current.Dispatcher.Invoke((Action)delegate
+				{
+					this.Background = msgColor.MessageData.Brush;
+				});
+			}
+        }
+	}
+}
